bd2adbe762044f3c7e77e7eaef6c7ef4cca93c9f
Jonathan Henrique <jonathan1_henrique@hotmail.com>
2021-12-31 16:01:27 -0300

    :tada: Arquitetura inicial do projeto

---
+++ financa/src/test/java/com/organizacao/financa/FinancaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.organizacao.financa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
